84562363323c9c3fe5907f7f4d6a1c1e8b7e914a
Vikalp Mishra <vikalpmishra783@gmail.com>
2025-12-18 19:09:24 +0530

    Fix: Check if AUTH env var is set before validating

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -35,7 +35,7 @@ class Web extends CI_Controller
 
     public function index($msg = null)
     {
-        if (! filter_var($_ENV['AUTH'], FILTER_VALIDATE_BOOLEAN)) {
+        if (isset($_ENV['AUTH']) && ! filter_var($_ENV['AUTH'], FILTER_VALIDATE_BOOLEAN)) {
             return redirect('web/auto_login');
         }
         $data = [
