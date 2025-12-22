065c6e7a36a1780ede3f013b4075963203332d32
Vikalp Mishra <vikalpmishra783@gmail.com>
2025-12-22 20:26:15 +0530

    Add AI Tools link to sidebar
    Adds a new list item to the category sidebar that links to the AI Tools page. This allows users to
    easily access AI tools from the dashboard.

--- application/views/web/dashboard/category_sidebar.php
+++ application/views/web/dashboard/category_sidebar.php
@@ -1,16 +1,19 @@
 <div class="home-side w-100">
     <div class="wmain_sidebar">
         <ul class="row justify-content-center <?= $user_type === 'Teacher' ? 'userTeacher' : '' ?>">
-            <?php foreach ($categories as $cat): ?>
-                <?php if ($cat->allow === 'Both' || $cat->allow === $user_type): ?>
+            <?php foreach ($categories as $cat) { ?>
+                <?php if ($cat->allow === 'Both' || $cat->allow === $user_type) { ?>
                     <li class="p-2 my-1 card col-5" id="active<?= $cat->id ?>" style="border-radius:0.5rem">
                         <a tab_id="<?= $cat->id ?>" class="new-search">
-                            <img src="<?= base_url('assets/cat_icons/' . $cat->icon) ?>" alt="">
+                            <img src="<?= base_url('assets/cat_icons/'.$cat->icon) ?>" alt="">
                             <?= $cat->name ?>
                         </a>
                     </li>
-                <?php endif; ?>
-            <?php endforeach; ?>
+                <?php } ?>
+            <?php } ?>
+            <li class="p-2 my-1 card col-5"" style="border-radius:0.5rem">
+              <a style="color: #fff;" href="https://ai.curiopublishing.in/skip-auth" target="_blank">AI Tools</a>
+            </li>
         </ul>
     </div>
 </div>
