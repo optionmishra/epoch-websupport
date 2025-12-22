42ab5bd927ddad14948dcaa3e21aef9319ccfffc
Vikalp Mishra <vikalpmishra783@gmail.com>
2025-12-22 17:35:03 +0530

    Refactor login page UI and assets
    Replaced the old login page structure with a new, more visually appealing design. This includes
    updated CSS, new image assets for the background and design elements, and a reorganized HTML
    structure. The overall layout has been shifted to a two-column design with decorative elements on
    the left and the login form on the right.

--- application/views/web/index.php
+++ application/views/web/index.php
@@ -2,165 +2,184 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title><?= $siteName ?></title>
-	<!-- Google Fonts -->
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-		rel="stylesheet" />
-	<link rel="stylesheet" href="<?= base_url('assets/revamp/fontawesome/css/all.min.css') ?>">
-	<link rel="stylesheet" href="<?= base_url('assets/revamp/css/style.css') ?>" />
-	<script src="<?= base_url('assets/revamp/js/script.js') ?>" defer></script>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title><?= $siteName ?></title>
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet" />
+  <link rel="stylesheet" href="<?= base_url(
+                                  "assets/revamp/fontawesome/css/all.min.css",
+                                ) ?>">
+  <link rel="stylesheet" href="<?= base_url("assets/revamp/css/style.css") ?>" />
+  <script src="<?= base_url("assets/revamp/js/script.js") ?>" defer></script>
 </head>
 
 <body data-theme="light" class="font-inter">
-	<div class="min-h-screen bg-background">
-		<main>
-			<div
-				class="container flex items-center justify-center min-h-screen px-1 mx-auto">
-				<div class="flex flex-col w-full h-full sm:flex-row">
-					<div
-						class="flex flex-col items-center w-full bg-white rounded-lg sm:w-1/2 sm:rounded-r-none 2xl:py-16">
-						<div
-							class="flex justify-center p-2 my-5 mb-10 bg-white rounded-md w-80 sm:w-96">
-							<img src="<?= base_url("assets/img/" . $logo['file_name']) ?>" alt="logo" />
-						</div>
-						<div class="text-center">
-							<h1 class="text-2xl font-semibold text-primary">
-								Welcome to <?= $siteName ?>
-							</h1>
-							<small class="font-semibold text-accent">
-								Your gateway to seamless web support starts
-								here!
-							</small>
-						</div>
-						<div class="w-2/3 my-10 2xl:w-1/2">
-							<form action="<?= base_url('web/process') ?>" method="post">
-								<div class="flex flex-col gap-1 mb-5">
-									<label
-										class="font-semibold text-secondary"
-										for="email">Email
-										<span class="text-sm text-accent">
-											*
-										</span>
-									</label>
-									<input
-										class="px-4 py-2 border border-gray-300 rounded-md focus:border-accent outline-accent"
-										id="email"
-										type="email"
-										name="username"
-										placeholder="Enter your email address"
-										required />
-								</div>
-								<div class="flex flex-col gap-1 mb-5">
-									<label
-										class="font-semibold text-secondary"
-										for="password">
-										Password
-										<span class="text-sm text-accent">
-											*
-										</span>
-									</label>
-									<input
-										class="px-4 py-2 border border-gray-300 rounded-md focus:border-accent outline-accent"
-										id="password"
-										type="password"
-										name="password"
-										placeholder="Enter your password"
-										required />
-								</div>
+  <div class="flex justify-center min-h-screen flex-wrap">
+    <!--Left-->
+    <div class="relative min-h-screen hidden md:flex items-end md:w-1/2 2xl:w-4/7">
+      <div class="min-h-screen h-full bg-[#094391] w-52"></div>
+      <div class="absolute top-[-4rem] left-64">
+        <img src="<?= base_url("assets/home/img/2.png") ?>" />
+      </div>
+      <div class="absolute bottom-0 left-4">
+        <img src="<?= base_url("assets/home/img/5.png") ?>" />
+      </div>
+      <div class="absolute top-10 right-[-4rem]">
+        <img src="<?= base_url("assets/home/img/7.png") ?>" />
+      </div>
+      <div class="ml-[10rem] relative">
+        <div class="absolute left-1/2 transform -translate-x-1/2 z-10 w-full">
+          <img src="<?= base_url("assets/home/img/9.png") ?>" />
+        </div>
+        <div class="z-20 relative">
+          <img src="<?= base_url("assets/home/img/1.png") ?>" />
+        </div>
+      </div>
+    </div>
+    <!--Right-->
+    <div class="w-full md:w-1/2 2xl:w-3/7 flex justify-center items-end relative overflow-hidden">
+      <div class="absolute scale-[0.6] top-[-6rem] right-[-6rem] z-10"><img src="<?= base_url("assets/home/img/3.png") ?>" /></div>
+      <div class="absolute scale-[0.6] bottom-[-4rem] right-[-2rem] sm:right-5 z-10"><img src="<?= base_url("assets/home/img/4.png") ?>" /></div>
+      <div class="flex flex-col my-5 2xl:mb-20 px-5 z-20">
+        <div
+          class="flex justify-center mb-5 rounded-md w-full">
+          <img class="max-h-48 max-w-48" src="<?= base_url("assets/img/" . $logo["file_name"]) ?>" alt="logo" />
+        </div>
+        <div class="text-center 2xl:my-10">
+          <h1 class="text-3xl font-bold">
+            <spam class="text-gray-800">Welcome to </spam>
+            <spam class="text-[#0267B1]"><?= $siteName ?></spam>
+          </h1>
+          <small class="font-semibold text-sm text-gray-500">
+            Your gateway to seamless web support starts
+            here!
+          </small>
+        </div>
+        <div class="mt-5 w-full sm:w-10/12 mx-auto">
+          <form action="<?= base_url("web/process") ?>" method="post">
+            <div class="flex flex-col gap-1 mb-5">
+              <label
+                class="font-semibold text-[#0267B1] ml-5"
+                for="email">Email
+                <span class="text-sm text-[#0267B1]">
+                  *
+                </span>
+              </label>
+              <input
+                class="px-4 py-3 rounded-full focus:border-[#0267B1] outline-[#0267B1] bg-[#E1F2F2]"
+                id="email"
+                type="email"
+                name="username"
+                required />
+              <!--placeholder="Enter your email address" -->
+            </div>
+            <div class="flex flex-col gap-1 mb-5">
+              <label
+                class="font-semibold text-[#0267B1] ml-5"
+                for="password">
+                Password
+                <span class="text-sm text-[#0267B1]">
+                  *
+                </span>
+              </label>
+              <input
+                class="px-4 py-3 rounded-full focus:border-[#0267B1] outline-[#0267B1] bg-[#E1F2F2]"
+                id="password"
+                type="password"
+                name="password"
+                required />
+              <!--placeholder="Enter your password" -->
+            </div>
 
-								<div
-									class="flex flex-col justify-between mb-5 sm:flex-row gap-y-2">
-									<div
-										class="flex items-center justify-center gap-1">
-										<input
-											class="w-4 h-4 cursor-pointer accent-accent"
-											type="checkbox"
-											id="showPassword" />
-										<label
-											class="cursor-pointer"
-											for="showPassword">Show Password</label>
-									</div>
-									<div
-										class="flex items-center justify-center">
-										<button onclick="forgotPasswordModal.showModal()" type="button" class="font-semibold text-secondary hover:text-accent">Forgot your Password?</button>
-									</div>
-								</div>
+            <div
+              class="flex flex-col justify-between mb-5 sm:flex-row gap-y-2">
+              <div
+                class="flex items-center justify-center gap-1">
+                <input
+                  class="w-5 h-5 cursor-pointer accent-[#0267B1] rounded-lg"
+                  type="checkbox"
+                  id="showPassword" />
+                <label
+                  class="cursor-pointer"
+                  for="showPassword">Show Password</label>
+              </div>
+              <div
+                class="flex items-center justify-center">
+                <button onclick="forgotPasswordModal.showModal()" type="button" class="font-semibold text-[#0267B1] hover:text-accent">Forgot your Password?</button>
+              </div>
+            </div>
 
-								<?php if ($this->session->flashdata('error')) : ?>
-									<div class="flex justify-center py-2 mb-5 ">
-										<strong class="text-red-600"><?= $this->session->flashdata('error'); ?></strong>
-									</div>
-									<?php $this->session->unset_userdata('error'); ?>
-								<?php endif; ?>
+            <?php if ($this->session->flashdata("error")) { ?>
+              <div class="flex justify-center py-2 mb-5 ">
+                <strong class="text-red-600"><?= $this->session->flashdata("error") ?></strong>
+              </div>
+              <?php $this->session->unset_userdata("error"); ?>
+            <?php } ?>
 
-								<div class="flex flex-col gap-3 mb-5">
-									<button type="submit" class="text-white bg-[#491820] hover:bg-accent focus:ring-4 focus:ring-accent font-semibold rounded-md py-2 px-4 focus:outline-none transition-colors duration-300 ease-in-out">Log In</button>
-								</div>
-							</form>
-						</div>
-						<div class="my-5 text-center">
-							<p class="text-secondary">
-								Don't have an account?
-							</p>
-							<div class="flex gap-5 my-5">
-								<button
-									class="px-4 py-2 font-semibold text-white transition-colors duration-300 ease-in-out rounded-md hover:bg-accent bg-secondary">
-									<a href="<?= base_url('teacher-registration') ?>">I'm a Teacher</a>
-								</button>
-								<button
-									class="px-4 py-2 font-semibold text-white transition-colors duration-300 ease-in-out rounded-md hover:bg-accent bg-secondary">
-									<a href="<?= base_url('student-registration') ?>">I'm a Student</a>
-								</button>
-							</div>
-						</div>
-						<!-- /container -->
-					</div>
+            <div class="flex justify-center my-6">
+              <button type="submit" class="text-white bg-[#0267B1] hover:bg-accent focus:ring-4 focus:ring-accent font-bold text-xl rounded-full py-3 px-10 focus:outline-none transition-colors duration-300 ease-in-out uppercase">Log In</button>
+            </div>
+          </form>
+        </div>
+        <div class="mb-5 text-center">
+          <p class="text-[#0267B1] font-semibold">
+            Don't have an account?
+          </p>
+          <div class="flex gap-5 my-5 justify-center">
+            <button
+              class="px-4 py-2 font-semibold text-white transition-colors duration-300 ease-in-out rounded-full hover:bg-accent bg-[#5CC6D0]">
+              <a href="<?= base_url("teacher-registration") ?>">I'm a Teacher</a>
+            </button>
+            <button
+              class="px-4 py-2 font-semibold text-white transition-colors duration-300 ease-in-out rounded-full hover:bg-accent bg-[#5CC6D0]">
+              <a href="<?= base_url("student-registration") ?>">I'm a Student</a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
-					<div
-						class="hidden bg-[url('../img/right-bg.png')] w-full bg-center bg-no-repeat bg-cover rounded-r-lg sm:block sm:w-1/2 right-bg"></div>
-				</div>
-			</div>
-		</main>
-	</div>
-
-	<!-- Forgot Password Modal -->
-	<dialog id="forgotPasswordModal" class="modal">
-		<div class="modal-box">
-			<h3 class="text-lg font-bold text-primary">Forgot your Password?</h3>
-			<form method="dialog">
-				<button class="absolute btn btn-sm btn-circle btn-ghost right-2 top-2">✕</button>
-			</form>
-			<form class="my-5" id="forgotPasswordForm" action="<?= base_url('admin_master/check_forgot') ?>" method="post">
-				<div class="flex flex-col gap-1 mb-5">
-					<label
-						class="font-semibold text-secondary"
-						for="recoveryEmail">Email
-						<span class="text-sm text-accent">
-							*
-						</span>
-					</label>
-					<input
-						class="px-4 py-2 border border-gray-300 rounded-md focus:border-accent outline-accent"
-						id="recoveryEmail"
-						type="email"
-						name="email"
-						placeholder="Enter your email address"
-						required />
-				</div>
-				<div class="my-5">
-					<p id="responseMessage"></p>
-				</div>
-				<div class="modal-action">
-					<button type="submit" class="text-white transition-colors duration-300 ease-in-out btn bg-primary hover:bg-accent">Submit</button>
-				</div>
-			</form>
-		</div>
-	</dialog>
+  <!-- Forgot Password Modal -->
+  <dialog id="forgotPasswordModal" class="modal">
+    <div class="modal-box">
+      <h3 class="text-lg font-bold text-primary">Forgot your Password?</h3>
+      <form method="dialog">
+        <button class="absolute btn btn-sm btn-circle btn-ghost right-2 top-2">✕</button>
+      </form>
+      <form class="my-5" id="forgotPasswordForm" action="<?= base_url(
+                                                            "admin_master/check_forgot",
+                                                          ) ?>" method="post">
+        <div class="flex flex-col gap-1 mb-5">
+          <label
+            class="font-semibold text-secondary"
+            for="recoveryEmail">Email
+            <span class="text-sm text-accent">
+              *
+            </span>
+          </label>
+          <input
+            class="px-4 py-2 border border-gray-300 rounded-md focus:border-accent outline-accent"
+            id="recoveryEmail"
+            type="email"
+            name="email"
+            placeholder="Enter your email address"
+            required />
+        </div>
+        <div class="my-5">
+          <p id="responseMessage"></p>
+        </div>
+        <div class="modal-action">
+          <button type="submit" class="text-white transition-colors duration-300 ease-in-out btn bg-primary hover:bg-accent">Submit</button>
+        </div>
+      </form>
+    </div>
+  </dialog>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+
+require_once __DIR__.'/vendor/autoload.php';
 
 /**
  * CodeIgniter
@@ -28,13 +29,14 @@ require_once __DIR__ . '/vendor/autoload.php';
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- * @package	CodeIgniter
  * @author	EllisLab Dev Team
  * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
  * @copyright	Copyright (c) 2014 - 2018, British Columbia Institute of Technology (http://bcit.ca/)
  * @license	http://opensource.org/licenses/MIT	MIT License
+ *
  * @link	https://codeigniter.com
  * @since	Version 1.0.0
+ *
  * @filesource
  */
 
@@ -70,28 +72,27 @@ define('ENVIRONMENT', $_SERVER['HTTP_HOST'] == 'localhost' ? 'development' : 'pr
 $envFile = '.env';
 
 switch (ENVIRONMENT) {
-	case 'development':
-		error_reporting(1);
-		ini_set('display_errors', 1);
-		$envFile = '.env.development';
-		break;
+    case 'development':
+        error_reporting(1);
+        ini_set('display_errors', 1);
+        $envFile = '.env.development';
+        break;
 
-	case 'testing':
-	case 'production':
-		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>=')) {
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		} else {
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
-		break;
+    case 'testing':
+    case 'production':
+        ini_set('display_errors', 1);
+        if (version_compare(PHP_VERSION, '5.3', '>=')) {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+        } else {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+        }
+        break;
 
-	default:
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'The application environment is not set correctly.';
-		break;
+    default:
+        header('HTTP/1.1 503 Service Unavailable.', true, 503);
+        echo 'The application environment is not set correctly.';
+        break;
 }
-
 
 // Load the appropriate .env file
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__, $envFile);
@@ -139,7 +140,6 @@ $application_folder = 'application';
  */
 $view_folder = '';
 
-
 /*
  * --------------------------------------------------------------------
  * DEFAULT CONTROLLER
@@ -169,7 +169,6 @@ $view_folder = '';
 // The controller function you wish to be called.
 // $routing['function']	= '';
 
-
 /*
  * -------------------------------------------------------------------
  *  CUSTOM CONFIG VALUES
@@ -186,8 +185,6 @@ $view_folder = '';
  */
 // $assign_to_config['name_of_config_item'] = 'value of config item';
 
-
-
 // --------------------------------------------------------------------
 // END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
 // --------------------------------------------------------------------
@@ -200,25 +197,25 @@ $view_folder = '';
 
 // Set the current directory correctly for CLI requests
 if (defined('STDIN')) {
-	chdir(dirname(__FILE__));
+    chdir(dirname(__FILE__));
 }
 
-if (($_temp = realpath($system_path)) !== FALSE) {
-	$system_path = $_temp . DIRECTORY_SEPARATOR;
+if (($_temp = realpath($system_path)) !== false) {
+    $system_path = $_temp.DIRECTORY_SEPARATOR;
 } else {
-	// Ensure there's a trailing slash
-	$system_path = strtr(
-		rtrim($system_path, '/\\'),
-		'/\\',
-		DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-	) . DIRECTORY_SEPARATOR;
+    // Ensure there's a trailing slash
+    $system_path = strtr(
+        rtrim($system_path, '/\\'),
+        '/\\',
+        DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+    ).DIRECTORY_SEPARATOR;
 }
 
 // Is the system path correct?
-if (!is_dir($system_path)) {
-	header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-	echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: ' . pathinfo(__FILE__, PATHINFO_BASENAME);
-	exit(3); // EXIT_CONFIG
+if (! is_dir($system_path)) {
+    header('HTTP/1.1 503 Service Unavailable.', true, 503);
+    echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
+    exit(3); // EXIT_CONFIG
 }
 
 /*
@@ -233,62 +230,62 @@ define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 define('BASEPATH', $system_path);
 
 // Path to the front controller (this file) directory
-define('FCPATH', dirname(__FILE__) . DIRECTORY_SEPARATOR);
+define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
 
 // Name of the "system" directory
 define('SYSDIR', basename(BASEPATH));
 
 // The path to the "application" directory
 if (is_dir($application_folder)) {
-	if (($_temp = realpath($application_folder)) !== FALSE) {
-		$application_folder = $_temp;
-	} else {
-		$application_folder = strtr(
-			rtrim($application_folder, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-		);
-	}
-} elseif (is_dir(BASEPATH . $application_folder . DIRECTORY_SEPARATOR)) {
-	$application_folder = BASEPATH . strtr(
-		trim($application_folder, '/\\'),
-		'/\\',
-		DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-	);
+    if (($_temp = realpath($application_folder)) !== false) {
+        $application_folder = $_temp;
+    } else {
+        $application_folder = strtr(
+            rtrim($application_folder, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+        );
+    }
+} elseif (is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR)) {
+    $application_folder = BASEPATH.strtr(
+        trim($application_folder, '/\\'),
+        '/\\',
+        DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+    );
 } else {
-	header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-	echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: ' . SELF;
-	exit(3); // EXIT_CONFIG
+    header('HTTP/1.1 503 Service Unavailable.', true, 503);
+    echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+    exit(3); // EXIT_CONFIG
 }
 
-define('APPPATH', $application_folder . DIRECTORY_SEPARATOR);
+define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
 
 // The path to the "views" directory
-if (!isset($view_folder[0]) && is_dir(APPPATH . 'views' . DIRECTORY_SEPARATOR)) {
-	$view_folder = APPPATH . 'views';
+if (! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR)) {
+    $view_folder = APPPATH.'views';
 } elseif (is_dir($view_folder)) {
-	if (($_temp = realpath($view_folder)) !== FALSE) {
-		$view_folder = $_temp;
-	} else {
-		$view_folder = strtr(
-			rtrim($view_folder, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-		);
-	}
-} elseif (is_dir(APPPATH . $view_folder . DIRECTORY_SEPARATOR)) {
-	$view_folder = APPPATH . strtr(
-		trim($view_folder, '/\\'),
-		'/\\',
-		DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-	);
+    if (($_temp = realpath($view_folder)) !== false) {
+        $view_folder = $_temp;
+    } else {
+        $view_folder = strtr(
+            rtrim($view_folder, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+        );
+    }
+} elseif (is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR)) {
+    $view_folder = APPPATH.strtr(
+        trim($view_folder, '/\\'),
+        '/\\',
+        DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+    );
 } else {
-	header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-	echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: ' . SELF;
-	exit(3); // EXIT_CONFIG
+    header('HTTP/1.1 503 Service Unavailable.', true, 503);
+    echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+    exit(3); // EXIT_CONFIG
 }
 
-define('VIEWPATH', $view_folder . DIRECTORY_SEPARATOR);
+define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
 
 /*
  * --------------------------------------------------------------------
@@ -297,4 +294,4 @@ define('VIEWPATH', $view_folder . DIRECTORY_SEPARATOR);
  *
  * And away we go...
  */
-require_once BASEPATH . 'core/CodeIgniter.php';
+require_once BASEPATH.'core/CodeIgniter.php';
